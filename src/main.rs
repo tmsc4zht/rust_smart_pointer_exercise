b467c82ea8b4ec4830deb94f309321b706b11492
tmsc4zht <79966690+tmsc4zht@users.noreply.github.com>
2021-03-12 23:06:01 +0900

    add comments

--- src/main.rs
+++ src/main.rs
@@ -33,17 +33,17 @@ fn main() {
     // bかスコープから抜けるとき、メモリの開放が起きる。
     println!("b = {}", b);
 
+    // Boxの参照外し演算子について
     let x = 5;
     let y = Box::new(x);
-
     assert_eq!(x, 5);
-    // yはポインタのように振る舞うので、参照外し演算子が必要。
-    assert_eq!(*y, 5);
+    assert_eq!(*y, 5); // yはポインタのように振る舞うので、参照外し演算子が必要。
 
+    // スマートポインタの自前実装について
     let y = MyBox::new(x);
     assert_eq!(*y, 5);
 
+    // Consの実行テスト
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
-
     println!("{:?}", list);
 }
