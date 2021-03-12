8b2dcde54d36e52b16c1f9a4a36b086bdc6d811c
tmsc4zht <79966690+tmsc4zht@users.noreply.github.com>
2021-03-12 20:38:16 +0900

    List implemented

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
+// Cons, Nilはenum Listの列挙子としてここで「定義」している
+#[derive(Debug)]
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+use List::{Cons, Nil};
+
 fn main() {
     let b = Box::new(5);
     // 値5はヒープ領域に確保されている
     // bかスコープから抜けるとき、メモリの開放が起きる。
-    println!("b = {}", b)
+    println!("b = {}", b);
+
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    println!("{:?}", list);
 }
