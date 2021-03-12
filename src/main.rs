267ea60acb2c826ed431b6f8f8ef7d1cf4a4717f
tmsc4zht <79966690+tmsc4zht@users.noreply.github.com>
2021-03-12 22:59:56 +0900

    MyBox implemented

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::ops::Deref;
+
 // Cons, Nilはenum Listの列挙子としてここで「定義」している
 #[derive(Debug)]
 enum List {
@@ -7,11 +9,39 @@ enum List {
 
 use List::{Cons, Nil};
 
+// Boxは1要素のタプルとして実装できる。
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+// スマートポインタとして振る舞うため参照外し演算子用のメソッドが必要
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
 fn main() {
     let b = Box::new(5);
     // 値5はヒープ領域に確保されている
     // bかスコープから抜けるとき、メモリの開放が起きる。
     println!("b = {}", b);
+
+    let x = 5;
+    let y = Box::new(x);
+
+    assert_eq!(x, 5);
+    // yはポインタのように振る舞うので、参照外し演算子が必要。
+    assert_eq!(*y, 5);
+
+    let y = MyBox::new(x);
+    assert_eq!(*y, 5);
 
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
 
