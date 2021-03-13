8fe998e49bddbc2717f5bb6e27fb4ce744f5570b
tmsc4zht <79966690+tmsc4zht@users.noreply.github.com>
2021-03-13 11:24:32 +0900

    参照カウント方式のテスト

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::ops::Deref;
+use std::rc::Rc;
 
 // Cons, Nilはenum Listの列挙子としてここで「定義」している
 #[derive(Debug)]
 enum List {
-    Cons(i32, Box<List>),
+    Cons(i32, Rc<List>),
     Nil,
 }
 
@@ -68,8 +69,23 @@ fn main() {
     hello(&m);
 
     // Consの実行テスト
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    let list = Cons(1, Rc::new(Cons(2, Rc::new(Cons(3, Rc::new(Nil))))));
     println!("{:?}", list);
+
+    // Consの実行テスト（データ共有）
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    // a生成後のカウント = {}
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let _b = Cons(3, Rc::clone(&a));
+    // b生成後のカウント = {}
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let _c = Cons(4, Rc::clone(&a));
+        // c生成後のカウント = {}
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    // cがスコープを抜けた後のカウント = {}
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 
     // Dropの実行テスト
     let _c = CustomSmartPointer {
